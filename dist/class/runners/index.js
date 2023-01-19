de3ef10a3b54b3a48b32027223e640aa492c588b
JediKunnow <filipposofi@Filippos-MacBook-Pro.local>
2023-01-19 23:31:35 +0200

    examples

--- dist/class/runners/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MasterChefRunner = void 0;
-var MasterChefRunner_1 = require("./MasterChefRunner");
-Object.defineProperty(exports, "MasterChefRunner", { enumerable: true, get: function () { return MasterChefRunner_1.MasterChefRunner; } });
-//# sourceMappingURL=index.js.map
